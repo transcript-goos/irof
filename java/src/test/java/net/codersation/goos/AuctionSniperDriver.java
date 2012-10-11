eaa611834cae2d24a0ec701ee32a91576622a1e4
irof <irof@javaring.info>
2012-10-11 23:15:24 +0900

    p.91 AuctionSniperDriver

--- java/src/test/java/net/codersation/goos/AuctionSniperDriver.java
+++ java/src/test/java/net/codersation/goos/AuctionSniperDriver.java
@@ -1,19 +1,22 @@
 package net.codersation.goos;
 
-public class AuctionSniperDriver {
+import static org.hamcrest.CoreMatchers.equalTo;
 
-	public AuctionSniperDriver(int i) {
-		// TODO Auto-generated constructor stub
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+public class AuctionSniperDriver extends JFrameDriver {
+
+	@SuppressWarnings("unchecked")
+	public AuctionSniperDriver(int timeoutMillis) {
+		super(new GesturePerformer(), JFrameDriver.topLevelFrame(named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
+				new AWTEventQueueProber(timeoutMillis, 100));
 	}
 
-	public void showsSniperStatus(String status) {
-		// TODO Auto-generated method stub
-		
+	@SuppressWarnings("unchecked")
+	public void showsSniperStatus(String statusText) {
+		new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
 	}
-
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- java/src/test/java/net/codersation/goos/Main.java
+++ java/src/test/java/net/codersation/goos/Main.java
@@ -2,6 +2,9 @@ package net.codersation.goos;
 
 public class Main {
 
+	public static final String MAIN_WINDOW_NAME = null;
+	public static final String SNIPER_STATUS_NAME = null;
+
 	public static void main(String xmppHostname, String sniperId, String sniperPassword, Object itemId) {
 		// TODO Auto-generated method stub
 		
