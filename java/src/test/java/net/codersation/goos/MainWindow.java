eaaf9cc336402f57994f4f0e681b9364bb578202
irof <irof@hogedriven.net>
2012-11-07 22:10:05 +0900

    P.98 MainWindowにJLabel追加
    次はメッセージで落ちてる

--- java/src/test/java/net/codersation/goos/MainWindow.java
+++ java/src/test/java/net/codersation/goos/MainWindow.java
@@ -1,15 +1,30 @@
 package net.codersation.goos;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 public class MainWindow extends JFrame {
 
 	private static final String MAIN_WINOW_NAME = null;
+	private static final String STATUS_JOINING = null;
+	private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
 	public MainWindow() {
 		super("Auction Sniper");
 		setName(MAIN_WINOW_NAME);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(sniperStatus);
+		pack();
 		setVisible(true);
+	}
+
+	private JLabel createLabel(String initialText) {
+		JLabel result = new JLabel(initialText);
+		result.setName(Main.SNIPER_STATUS_NAME);
+		result.setBorder(new LineBorder(Color.BLACK));
+		return result;
 	}
 }
