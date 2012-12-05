ce20090f62bf29cdaed3b6b2c7d909fbaa8c857a
irof <irof@hogedriven.net>
2012-12-05 23:06:55 +0900

    P.114 The First Unit Test

---
+++ java/src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -0,0 +1,14 @@
+package test.auctionsniper;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuctionMessageTranslatorTest {
+
+	@Test
+	public void notifiesAuctionClosedWhenCloseMessageRecieved() throws Exception {
+		
+	}
+}
