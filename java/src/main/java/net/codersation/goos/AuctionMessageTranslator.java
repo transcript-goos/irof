cc936d2aa2d15fd9bca26788abbf54c9916dc244
irof <irof@hogedriven.net>
2012-12-05 23:10:58 +0900

    P.114 The First Unit Test 2

---
+++ java/src/main/java/net/codersation/goos/AuctionMessageTranslator.java
@@ -0,0 +1,14 @@
+package net.codersation.goos;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator implements MessageListener {
+
+	public void processMessage(Chat unusedChat, Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- java/src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ java/src/test/java/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -3,12 +3,22 @@ package test.auctionsniper;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import net.codersation.goos.AuctionMessageTranslator;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
 import org.junit.Test;
 
 public class AuctionMessageTranslatorTest {
 
+	public static final Chat UNUSED_CHAT = null;
+	private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
+
 	@Test
 	public void notifiesAuctionClosedWhenCloseMessageRecieved() throws Exception {
-		
+		Message message = new Message();
+		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
+
+		translator.processMessage(UNUSED_CHAT, message);
 	}
 }
